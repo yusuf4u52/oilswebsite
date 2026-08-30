42d4b31b1c2bd47584367ec80bf1c5ac81628909
yusuf4u52 <yusuf4u52@gmail.com>
2026-08-30 14:50:05 +0530

    feat: add close button to Toaster component for improved user interaction

--- frontend-next/app/providers.js
+++ frontend-next/app/providers.js
@@ -24,7 +24,7 @@ export default function Providers({ children }) {
       <AuthProvider>
         <CartProvider>
           {children}
-          <Toaster position="top-right" richColors />
+          <Toaster position="top-right" richColors closeButton />
         </CartProvider>
       </AuthProvider>
     </QueryClientProvider>
